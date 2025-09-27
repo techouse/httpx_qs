813721455b4caa035e041cbe957bfcda354e78d7
Klemen Tusar <techouse@gmail.com>
2025-09-27 17:25:12 +0100

    :bookmark: release v0.1.2
    ## 0.1.2
    
    * [DOCS] add Sphinx documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
+## 0.1.2
+
+* [DOCS] add Sphinx documentation
+
 ## 0.1.1
 
 * [CHORE] limit httpx to `>=0.28.1, <1.0.0`
 
 ## 0.1.0
 
-* [CHORE] Initial release.
+* [CHORE] initial release
--- src/httpx_qs/__init__.py
+++ src/httpx_qs/__init__.py
@@ -1,6 +1,6 @@
 """httpx-qs: A library for smart query string handling with httpx."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .enums.merge_policy import MergePolicy
 from .transporters import smart_query_strings
