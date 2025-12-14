121b2c8223eb3acc8670bab65a833dd2455ac49a
Klemen Tusar <techouse@gmail.com>
2025-12-14 13:07:08 +0000

    :bookmark: bump version to 0.2.1

--- src/httpx_qs/__init__.py
+++ src/httpx_qs/__init__.py
@@ -1,6 +1,6 @@
 """httpx-qs: A library for smart query string handling with httpx."""
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from .enums.merge_policy import MergePolicy
 from .transporters import smart_query_strings
